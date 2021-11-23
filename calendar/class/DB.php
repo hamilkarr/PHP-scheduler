7a00a610146ac8ea817591cb5f42028427132173
hamilkarr <86813319+hamilkarr@users.noreply.github.com>
2021-11-24 07:47:17 +0900

    Update DB.php

--- calendar/class/DB.php
+++ calendar/class/DB.php
@@ -2,9 +2,9 @@
 class DB extends PDO {
   public function __construct() {
     try {
-      $dsn = "mysql:host=localhost;dbname=hamilkarr";
-      $username = "hamilkarr";
-      $password = "aA!13579";
+      $dsn = "mysql:host=localhost;dbname=xxxxxx";
+      $username = "xxxxxx";
+      $password = "xxxxxx";
       parent::__construct($dsn, $username, $password);
     } catch (PDOException $e) {
       throw $e;
